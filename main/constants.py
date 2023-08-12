b80b72e8b81830a68e189090d8277334b72a784f
Alex Wang <iamalex11303@gmail.com>
2023-08-11 19:29:49 -0500

    added path to data folder

--- main/constants.py
+++ main/constants.py
@@ -1,8 +1,14 @@
 import os
 
+""" Name of the user data folder. """
+DATA_FOLDER_NAME: str = ".data"
+
 """ The relative path from cwd to the CONSTS.txt file. """
 os.makedirs(".data")
-REL_CNSTS_PATH: os.PathLike = os.path.join(".data", "CONSTS.txt")
+REL_CNSTS_PATH: os.PathLike = os.path.join(DATA_FOLDER_NAME, "CONSTS.txt")
 
 """ The global path to the constants file. """
 USR_CNSTS_PATH: os.PathLike = os.path.join(os.getcwd(), REL_CNSTS_PATH)
+
+""" The path to user-entered data. """
+DATA_FOLDER_PATH: os.PathLike = os.path.join(os.getcwd(), DATA_FOLDER_NAME)
