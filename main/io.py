742f5a7753813a28a54944a906d70ca3dc2a59fd
Alex Wang <iamalex11303@gmail.com>
2023-08-11 21:13:07 -0500

    added function to delete entries

--- main/io.py
+++ main/io.py
@@ -28,3 +28,19 @@ def make_entry(title: str) -> None:
 
     with open(os.path.join(con.DATA_FOLDER_PATH, title), "w") as file:
         file.write(date.date.today().strftime(format="%d/%m/%Y, %H:%M:%S"))
+
+def delete_entry(path: os.PathLike) -> None:
+    """
+    Deletes a given entry.
+
+    Parameters:
+        path (os.PathLike): A path to the entry to delete.
+
+    Raises:
+        RuntimeError: If the entry at the given path does not exist.
+    """
+
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        raise RuntimeError("Entry does not exist.")
