d5414ebba1821622393b1a83ca825bf4398b6bea
Alex Wang <iamalex11303@gmail.com>
2023-08-13 09:41:37 -0500

    creates CONSTS.txt if it does not exist.& C:/Python311/python.exe c:/Users/15043/Coding_Projects/PythonProjects/journal/main/journal.py

--- main/constants.py
+++ main/constants.py
@@ -3,10 +3,13 @@ import os
 """ Name of the user data folder. """
 DATA_FOLDER_NAME: str = ".data"
 
-""" The relative path from cwd to the CONSTS.txt file. """
+""" The relative path from cwd to the CONSTS.txt folder. """
 if not os.path.exists(DATA_FOLDER_NAME):
     os.makedirs(".data")
 REL_CNSTS_PATH: os.PathLike = os.path.join(DATA_FOLDER_NAME, "CONSTS.txt")
+if not os.path.exists(REL_CNSTS_PATH):
+    with open(REL_CNSTS_PATH, "x") as file:
+        pass
 
 """ The global path to the constants file. """
 USR_CNSTS_PATH: os.PathLike = os.path.join(os.getcwd(), REL_CNSTS_PATH)
