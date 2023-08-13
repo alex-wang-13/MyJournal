6b5c9008e4fd0cdf17a68fe0bd4517506cda1bbf
Alex Wang <iamalex11303@gmail.com>
2023-08-13 17:06:32 -0500

    dark mode on

--- main/journal.py
+++ main/journal.py
@@ -11,7 +11,7 @@ selection: str | None = None
 
 def refresh() -> None:
     sidebar.delete(0, tk.END)
-    print("hi")
+
     for path in data.entries.values():
         sidebar.insert(tk.END, path)
 
@@ -57,56 +57,58 @@ root = tk.Tk()
 root.state("zoomed")
 root.resizable(False, False)
 # Configure.
-root.configure(bg="lightgrey")
+root.configure(bg="grey25")
 root.title("MyJournal")
 
 # Create sidebar.
-sidebar = tk.Listbox(root, exportselection=False, width=50)
+sidebar = tk.Listbox(root, exportselection=False, bg="mistyrose4",fg="white", width=50)
 sidebar.bind("<<ListboxSelect>>", func=save_entry, add="+")
 sidebar.bind("<<ListboxSelect>>", func=load_entry, add="+")
 sidebar.pack(side=tk.LEFT, fill=tk.BOTH)
+sidebar.select_anchor(tk.END)
 refresh()
 
 # Top frame.
-top_frame = tk.Frame(root, bg="lightgrey")
+top_frame = tk.Frame(root, bg="grey25")
 top_frame.pack(side=tk.TOP, padx=10, pady=10, fill=tk.BOTH, expand=1)
 
 # Top left.
-topleft_frame = tk.Frame(top_frame, bg="lightgrey")
+topleft_frame = tk.Frame(top_frame, bg="grey25")
 topleft_frame.pack(side=tk.LEFT, padx=50, pady=50)
 
-add_entry_btn = tk.Button(topleft_frame, text="Add Entry", width=10)
+add_entry_btn = tk.Button(topleft_frame, text="Add Entry",fg="white", width=10)
 add_entry_btn.bind("<Button-1>", func=add_entry)
 add_entry_btn.pack(side=tk.TOP, pady=10)
-add_entry_btn.configure(bg="lightblue")
+add_entry_btn.configure(bg="lightsteelblue3")
 
 # Delete entry specifier.
-del_entry: tk.Entry = tk.Entry(topleft_frame, bg="lightgrey")
+del_entry: tk.Entry = tk.Entry(topleft_frame, bg="mistyrose4",fg="white")
 del_entry.pack(side=tk.BOTTOM)
 
-del_entry_btn = tk.Button(topleft_frame, text="Delete Entry", width=10)
+del_entry_btn = tk.Button(topleft_frame, text="Delete Entry",fg="white", width=10)
 del_entry_btn.bind("<Button-1>", func=delete_entry)
 del_entry_btn.pack(side=tk.BOTTOM, pady=10)
-del_entry_btn.configure(bg="lightpink")
+del_entry_btn.configure(bg="hotpink3")
 
 # Top right.
-topright_frame = tk.Frame(top_frame, bg="lightgrey")
+topright_frame = tk.Frame(top_frame, bg="grey25")
 topright_frame.pack(side=tk.RIGHT, padx=50, pady=50)
 
 # Search bar.
-search_label = tk.Label(topright_frame, text="Search phrase:",bg="lightgrey", width=15)
+search_label = tk.Label(topright_frame, text="Search phrase:",bg="grey25",fg="white", width=15)
 search_label.pack(side=tk.TOP)
 
 # Search prompt.
-search_box = tk.Text(topright_frame, height=1, width=20)
+search_box = tk.Text(topright_frame, bg="mistyrose4",fg="white", height=1, width=20)
 search_box.pack(side=tk.BOTTOM, pady=5)
 
 # Create bottom frame.
-bot_frame = tk.Frame(root, bg="lightgrey")
+bot_frame = tk.Frame(root, bg="grey25")
 bot_frame.pack(side=tk.BOTTOM, padx=50, pady=50)
 bot_frame.configure(bg="red")
 
-text_area = tk.Text(bot_frame)
+# Text area.
+text_area = tk.Text(bot_frame, bg="mistyrose4",fg="white")
 text_area.pack()
 
 root.mainloop()
